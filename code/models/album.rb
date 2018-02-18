1e57db007c579ee04bbf8f074c5ff65808d32b33
Jim Bevington <jim.bevington@hotmail.com>
2018-02-18 21:01:02 +0000

    added album.update() method

--- code/models/album.rb
+++ code/models/album.rb
@@ -2,7 +2,8 @@ require_relative('../db/sql_runner.rb')
 
 class Album
 
-  attr_reader :id, :title, :artist_id, :quantity
+  attr_reader :id
+  attr_accessor :title, :artist_id, :quantity
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
@@ -16,6 +17,13 @@ class Album
            VALUES ($1, $2, $3) RETURNING *"
     values = [@title, @artist_id, @quantity]
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE albums SET (title, artist_id, quantity) =
+           ($1, $2, $3) WHERE id = $4"
+    values = [@title, @artist_id, @quantity, @id]
+    SqlRunner.run(sql, values)
   end
 
 
