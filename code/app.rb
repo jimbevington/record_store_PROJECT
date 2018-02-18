f23cb20c2a117e60af525e461ef6a6a8c2698508
Jim Bevington <jim.bevington@hotmail.com>
2018-02-18 22:22:01 +0000

    got basic inventory screen showing. may redesign to create new joined table that can be sorted.

--- code/app.rb
+++ code/app.rb
@@ -1,6 +1,8 @@
 require('sinatra')
 require('sinatra/contrib/all')
 
+require_relative('./models/album.rb')
+require_relative('./models/artist.rb')
 require_relative('./controllers/album_controller.rb')
 require_relative('./controllers/artist_controller.rb')
 
@@ -10,5 +12,6 @@ end
 
 # NOT DONE YET
 get '/inventory' do
+  @albums = Album.all()
   erb(:inventory)
 end
