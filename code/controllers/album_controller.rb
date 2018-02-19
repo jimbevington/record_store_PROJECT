a2441310d2c6546a280ae549faa45f54e3a0b021
Jim Bevington <jim.bevington@hotmail.com>
2018-02-19 09:50:22 +0000

    added Create Artist page and artists/new and artists/ post method

--- code/controllers/album_controller.rb
+++ code/controllers/album_controller.rb
@@ -5,6 +5,18 @@ require('sinatra/contrib/all')
 require_relative('../models/album.rb')
 require_relative('../models/artist.rb')
 
+
+get '/albums/new' do
+  erb(:"albums/new")
+end
+
+post '/albums' do
+  artist_id = Artist.find_by_name(params('artist')).id
+  album = Album.new(params)
+  album.save()
+  redirect to("/albums/#{album.id}")
+end
+
 get '/albums/:id' do
   @album = Album.find_by_id(params['id'].to_i)
   erb( :"albums/view" )
--- code/controllers/artist_controller.rb
+++ code/controllers/artist_controller.rb
@@ -5,6 +5,16 @@ require('sinatra/contrib/all')
 require_relative('../models/album.rb')
 require_relative('../models/artist.rb')
 
+get '/artists/new' do
+  erb(:"artists/new")
+end
+
+post '/artists' do
+  artist = Artist.new(params)
+  artist.save()
+  redirect to("/artists/#{artist.id}")
+end
+
 get '/artists/:id' do
   @artist = Artist.find_by_id(params['id'].to_i)
   @albums = @artist.albums
