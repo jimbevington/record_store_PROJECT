c252cbe7468064e0b22444f8771e9eb90d6594cb
Jim Bevington <jim.bevington@hotmail.com>
2018-02-20 14:58:59 +0000

    changed artist.find_by_id method to return nil if artist not found'

--- code/models/artist.rb
+++ code/models/artist.rb
@@ -58,8 +58,13 @@ class Artist
   def self.find_by_id(id)
     sql = "SELECT * FROM artists WHERE id = $1"
     values = [id]
-    artist = SqlRunner.run(sql, values)[0]
-    return Artist.new(artist)
+    result = SqlRunner.run(sql, values)
+    artist = result.map{|artist| Artist.new(artist)}
+    if artist.length() > 0
+      return artist[0]
+    else
+      return nil
+    end
   end
 
   # could adjust to handle up/down case
