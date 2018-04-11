caee1ac8719488e2e28d09f279557d85905b985f
Jim Bevington <jim.bevington@hotmail.com>
2018-04-11 13:15:12 +0100

    updated todo list

--- code/app.rb
+++ code/app.rb
@@ -6,6 +6,7 @@ require_relative('models/artist.rb')
 require_relative('controllers/album_controller.rb')
 require_relative('controllers/artist_controller.rb')
 
+
 get '/' do
   @albums = Album.all()
   erb(:index)
